32906bb181d19186851a96d1bdd6e4f0bb6f4798
gilmer <pradavelasque@gmail.com>
2018-09-01 15:18:26 -0400

    config eurekaserver

--- src/main/java/com/prada/gilmer/discoveryserver/DiscoveryServerApplication.java
+++ src/main/java/com/prada/gilmer/discoveryserver/DiscoveryServerApplication.java
@@ -2,8 +2,9 @@ package com.prada.gilmer.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
